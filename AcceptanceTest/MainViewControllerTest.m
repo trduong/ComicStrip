2d7c319ffbe730befb041003115c05ca88c57c1d
trduong <DuongNG@duongs-air.austin.ibm.com>
2015-09-01 15:09:11 -0500

    KIF

--- AcceptanceTest/MainViewControllerTest.m
+++ AcceptanceTest/MainViewControllerTest.m
@@ -6,7 +6,44 @@
 //  Copyright (c) 2015 Atmarkcafe. All rights reserved.
 //
 
-#import "MainViewControllerTest.h"
+#import <UIKit/UIKit.h>
+#import <KIF/KIF.h>
+
+@interface MainViewControllerTest : KIFTestCase
+
+@end
 
 @implementation MainViewControllerTest
+
+
+- (void)beforeAll
+{
+}
+
+- (void)beforeEach
+{
+    
+}
+
+- (void)afterEach
+{
+    
+}
+
+- (void)afterAll
+{
+    
+}
+
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
 @end
